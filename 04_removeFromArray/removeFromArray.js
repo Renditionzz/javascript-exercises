d6fc54d3672c249581ddc489e287aaabbc6ea752
Renditionzz <99329650+Renditionzz@users.noreply.github.com>
2023-04-09 02:03:57 -0700

    Complete removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+/*
+    Goal: Implement a function that takes an array and other arguments and removes those arguments
+    from the array.
 
+    removeFromArray()
+        for each argument in the function (skip the first argument because that's our array)
+            if the argument is a number
+                look through the array and remove any of that number from the array
+        return the array
+*/
+
+
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++){
+        array = array.filter(word => word !== arguments[i]);
+    }
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
