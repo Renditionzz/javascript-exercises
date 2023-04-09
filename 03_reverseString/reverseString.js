807cb77c219b42ad9b62c84573189ab865238f12
Renditionzz <99329650+Renditionzz@users.noreply.github.com>
2023-04-09 01:44:36 -0700

    Complete reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+/*
+    Goal: write a function that takes a string and returns the reverse of the string.
 
+    function(string)
+        create a storage for the new string
+        for each position in the string
+            get the last character of the string, minus what iteration we're on
+            add it to our new string
+        return new string
+            
+*/
+
+const reverseString = function(string) {
+    let reversed = "";
+    for (let i = 0; i < string.length; i++){
+        let character = string.charAt(string.length - 1 - i);
+        reversed += character;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
