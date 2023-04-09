da52af01bf25309bd4d580b4d26fd3e50275f7d7
Renditionzz <99329650+Renditionzz@users.noreply.github.com>
2023-04-09 02:24:28 -0700

    Complete leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+/*
+    Create a function to determine if a year is a leap year or not.
 
+    Leap year rules:
+        divisible by 4
+        NOT divisible by 100
+        divisible by 400
+
+    leapYear(year)
+        if year % 4 is 0 and year % 100 != 0 and year % 400 is 0
+            it is a leap year
+        it is not a leap year
+*/
+
+const leapYears = function(year) {
+    if (year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0)){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
