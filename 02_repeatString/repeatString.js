adcabe40de2b77bc7f73318ed23b105d950781b7
Renditionzz <99329650+Renditionzz@users.noreply.github.com>
2023-04-09 01:34:56 -0700

    Finish repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+/*
+    Goal: Repeat a string any amount of times.
+    
+    create a location to store the new string.
+    for each iteration up to the number of times allowed
+        add the string to the new string
+    return the new string
+*/
 
+
+const repeatString = function(string, number) {
+    if (number < 0){
+        return "ERROR";
+    } 
+    
+    let result = "";
+    for (let i = 0; i < number; i++){
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
